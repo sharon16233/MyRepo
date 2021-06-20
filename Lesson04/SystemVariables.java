8108a79ef58dbcc9733aca3c28db467412eb98f0
sharon16233 <51725020+sharon16233@users.noreply.github.com>
2021-06-20 14:38:26 +0300

    Lesson04Commit01

---
+++ Lesson04/SystemVariables.java
@@ -0,0 +1,23 @@
+package Lesson04;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class SystemVariables {
+
+    private static String myDataPath = "\\resources\\myData.properties";
+
+    public static void main(String[] args) throws IOException {
+        String appConfigPath = System.getProperty("user.dir") + myDataPath;
+        Properties prop = new Properties();
+        prop.load(new FileInputStream(appConfigPath));
+
+        System.out.println(prop.getProperty("browser"));
+
+
+    }
+
+}
